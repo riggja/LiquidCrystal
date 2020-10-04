02b1ae6d207057ca9ec890b489a1aaee1ca6440a
Elizabeth Ventura <ejventuraflores@gmail.com>
2020-10-04 12:00:32 -0700

    reformat

--- test/TestClass.cpp
+++ test/TestClass.cpp
@@ -15,37 +15,37 @@ unittest(getRows) {
 }
 
 unittest(clearBuffer) {
-  //create lcd object
+  // create lcd object
   LiquidCrystal_Test lcd(rs, enable, d4, d5, d6, d7);
-  //get currently displayed lines
+  // get currently displayed lines
   std::vector<std::string> lines = lcd.getLines();
-  //verify that display contains 1 empty line
+  // verify that display contains 1 empty line
   assertEqual(1, lines.size());
   assertEqual(0, lines.at(0).length());
 
-  //reset lcd to have two lines
+  // reset lcd to have two lines
   lcd.begin(16, 2);
-  //verify that begin clears the display
+  // verify that begin clears the display
   lines = lcd.getLines();
-  //verify that display contains 2 empty lines
+  // verify that display contains 2 empty lines
   assertEqual(2, lines.size());
   assertEqual(0, lines.at(0).length());
   assertEqual(0, lines.at(1).length());
 
-  //write something to display
+  // write something to display
   lcd.print("hello world");
 
-  //verify display not empty
+  // verify display not empty
   lines = lcd.getLines();
   assertEqual(2, lines.size());
   // assertEqual(11, lines.at(0).length());
   // assertEqual("hello world", lines.at(0));
   assertEqual(0, lines.at(1).length());
 
-  //clear display
+  // clear display
   lcd.clear();
 
-  //verify display is empty
+  // verify display is empty
   lines = lcd.getLines();
   assertEqual(2, lines.size());
   assertEqual(0, lines.at(0).length());
